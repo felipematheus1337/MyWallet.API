cd531396ff31a06253ada1828e9655ef27a27eff
felipematheus1337 <lipehbr@gmail.com>
2025-04-27 14:19:15 -0300

    adicionado mapper

---
+++ Mapper/DTOMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MyWalletApi.Domain;
+using MyWalletApi.DTOs;
+
+namespace MyWalletApi.Mapper;
+
+public class DTOMappingProfile : Profile
+{
+
+    public DTOMappingProfile() {
+        CreateMap<Usuario, CreateUserDto>().ReverseMap();
+        CreateMap<Transacao, CreateTransacaoDto>().ReverseMap();
+        CreateMap<Transacao, TransacaoResponseDto>().ReverseMap();
+        CreateMap<Carteira, CarteiraResponseDto>().ReverseMap();
+    }  
+}
